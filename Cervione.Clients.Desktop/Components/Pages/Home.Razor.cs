c13729b0dabd6a0256e552ecea3c4745c36deff7
belmo <belmokhtarpatrick@gmail.com>
2025-07-27 03:39:29 +0200

    style: sort namespaces

--- Cervione.Clients.Desktop/Components/Pages/Home.Razor.cs
+++ Cervione.Clients.Desktop/Components/Pages/Home.Razor.cs
@@ -1,9 +1,12 @@
 ﻿using System.Net.Http.Json;
-using Community.Blazor.MapLibre;
-using Community.Blazor.MapLibre.Models.Control;
 
 using Cervione.Core.Models.Groups;
 using Device = Cervione.Core.Models.Devices.Device;
+
+using Community.Blazor.MapLibre;
+using Community.Blazor.MapLibre.Models;
+using Community.Blazor.MapLibre.Models.Control;
+using Community.Blazor.MapLibre.Models.Marker;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
@@ -27,13 +30,35 @@ public sealed partial class Home : ComponentBase
     private MapOptions Options => new MapOptions
     {
         Style = "style.json",
-        MinZoom = 1,
-        MaxZoom = 23
+        MinZoom = 2,
+        MaxZoom = 23,
+        CanvasContextAttributes = new WebGLContextAttributes
+        {
+            Antialias = true,
+            ContextType = "webgl2"
+        }
     };
 
     protected override async Task OnInitializedAsync()
     {
         Devices = await _http.GetFromJsonAsync<List<Device>>("/devices/me");
+    }
+
+    private async Task OnMapLoaded()
+    {
+        foreach (var device in Devices)
+        {
+            var options = new MarkerOptions
+            {
+                OpacityWhenCovered = "0",
+                Extensions = new MarkerOptionsExtensions
+                {
+                    HtmlContent = $"<img src='https://i.pravatar.cc/300' class='rounded-circle border border-3 border-white shadow-lg' alt='{device.Name}' weight='40' height='40' />" 
+                }
+            };
+
+            await _map.AddMarker(options, new LngLat(device.Position.Longitude, device.Position.Latitude), new Guid(device.Id));
+        }
     }
 
     private async Task OnStyleLoaded()
