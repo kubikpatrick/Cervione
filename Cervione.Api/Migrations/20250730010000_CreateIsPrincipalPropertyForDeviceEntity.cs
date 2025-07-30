0eb36d74dd041c59d7c52d90d20f382bcae405b8
belmo <belmokhtarpatrick@gmail.com>
2025-07-31 01:58:32 +0200

    feat: add `IsPrincipal` property to `Device` entity with migration

---
+++ Cervione.Api/Migrations/20250730010000_CreateIsPrincipalPropertyForDeviceEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cervione.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIsPrincipalPropertyForDeviceEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrincipal",
+                table: "Devices",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrincipal",
+                table: "Devices");
+        }
+    }
+}
