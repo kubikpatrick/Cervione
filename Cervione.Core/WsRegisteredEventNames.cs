cb3244dd90cc84f9a35607b2c75dacd6539363c4
belmo <belmokhtarpatrick@gmail.com>
2025-08-01 02:02:48 +0200

    refactor: rename `UpdatePosition` to `BroadcastPosition` in `WsRegisteredEventNames`

--- Cervione.Core/WsRegisteredEventNames.cs
+++ Cervione.Core/WsRegisteredEventNames.cs
@@ -2,5 +2,5 @@ namespace Cervione.Core;
 
 public readonly struct WsRegisteredEventNames
 {
-    public const string UpdatePosition = "UPDATE_POSITION";
+    public const string BroadcastPosition = "BROADCAST_POSITION";
 }
